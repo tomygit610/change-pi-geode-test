4364521498eddc179f836d781cd059f34b6f8dc0
tomygit610 <tomyroo2016@gmail.com>
2026-08-19 13:45:26 -0400

    Refactor comments and clean up code in main.cpp
    Updated comments for clarity and removed unnecessary code.

--- src/main.cpp
+++ src/main.cpp
@@ -5,15 +5,9 @@ using namespace geode::prelude;
 
 class $modify(PlayerObject) {
     void update(float dt) {
-        // Retrieve the custom Pi value set by the user in the settings menu
+        // Retrieve the custom Pi value set by the user in settings
         double customPi = Mod::get()->getSettingValue<double>("pi-value");
-
-        // The reset button in Geode settings automatically reverts customPi back to 3.14159
 
         PlayerObject::update(dt);
     }
 };
-
-$execute {
-    log::info("Custom Pi Mod loaded successfully!");
-}
